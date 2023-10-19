fff0d9f71dda861ea4044092d33960a7cdbd6d26
Hugo MG Sung <personar95@naver.com>
2023-10-20 02:57:44 +0900

    안올라가??

---
+++ chap03/src/main/java/com/hugo83/FirstServlet.java
@@ -0,0 +1,30 @@
+package com.hugo83;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "firstServlet", value = "/firstServlet")
+public class FirstServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Servlet!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ chap03/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <title>IntelliJ JSP</title>
+</head>
+<body>
+<h1><%= "Hello World!" %></h1>
+
+<div>
+    <a href="./firstServlet">First Servlet</a>
+</div>
+</body>
+</html>
